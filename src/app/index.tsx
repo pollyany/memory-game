6583719932e1e64b6a2d2d50973ec2439f56b3a4
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-03-03 16:21:25 -0300

    Configurando store de autenticação

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,12 @@
-import { Redirect } from 'expo-router'
+import { useAuthStore } from '@/shared/stores/auth.store'
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return <Redirect href="/(public)/login" />
+  const { user } = useAuthStore();
+
+  if (user) {
+    return <Redirect href="/(private)/home" />;
+  }
+
+  return <Redirect href="/(public)/login" />;
 }
