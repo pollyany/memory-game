2ee7bde43537b514524a037e9e1df11a59405c84
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-03-05 10:44:34 -0300

    Criando Header

---
+++ src/screens/Home/Home.view.tsx
@@ -0,0 +1,28 @@
+import { colors } from "@/constants/colors";
+import { AppText } from "@/shared/components/AppText";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { HomeHeader } from "./components/HomeHeader";
+
+export const HomeView = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        {" "}
+        <HomeHeader />
+        <AppText>Home</AppText>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.grayscale.gray700,
+  },
+  content: {
+    flex: 1,
+    paddingHorizontal: 24,
+  },
+});
